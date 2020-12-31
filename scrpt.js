0cbb7f2b1198a51b70e2d52133034be90e7f13e7
zaynab <zeinabheidarzadeh1377@gmial.com>
2020-12-31 08:24:13 -0800

    sort drs

--- scrpt.js
+++ scrpt.js
@@ -1,20 +1,21 @@
 
-function displayData() {
+function displayDrs() {
   
    fetch('https://intense-ravine-40625.herokuapp.com/doctors')
    .then(response => response.json())
    .then(data => {
      for (var key in data) {
 
-         var itm = document.getElementById("first");
-         var cln = itm.cloneNode(true);
-         cln.setAttribute("id", key);
-         document.getElementById("drs").appendChild(cln);
+        var itm = document.getElementById("first");
+        var cln = itm.cloneNode(true);
+        cln.setAttribute("id", key);
+        document.getElementById("drs").appendChild(cln);
 
         for (var key2 in data[key]){
             if (key2 != 'id'){
                 var dr = document.getElementById(key);
-                var child = dr.querySelector("#"+key2);
+                dr.style.display = 'flex';
+                var child = dr.querySelector("."+key2);
                 if (key2 == 'avatar'){
                     child.setAttribute('src',data[key][key2]);
                 }
@@ -27,12 +28,24 @@ function displayData() {
             }
            
         }
-    
-
-         
      }
    });
-   document. document.getElementById('first')
   
 }
 
+function sortDrs() {
+
+    var list = document.getElementById('drs');
+
+    var nodesToSort = list.querySelectorAll('.my-dr');
+    Array.prototype.map.call(nodesToSort, function(node) {
+        return {
+            node: node,
+            relevantText: node.querySelector('.user_percent').textContent
+        };
+    }).sort(function(a, b) {
+        return b.relevantText - a.relevantText;
+    }).forEach(function(item) {
+    list.appendChild(item.node);
+});
+}
